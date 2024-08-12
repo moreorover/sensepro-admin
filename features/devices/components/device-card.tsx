65b45b7b2449c2591526ad8f0038b3c192fabded
moreorover <moreorover@null.computer>
2024-08-12 22:29:15 +0100

    device card

--- features/devices/components/device-card.tsx
+++ features/devices/components/device-card.tsx
@@ -9,7 +9,8 @@ import {
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { DeviceShemaType } from "@/lib/apiSchema";
-import { Copy } from "lucide-react";
+import { DeviceType } from "@prisma/client";
+import { Cctv, Copy, Radar, TowerControl, Videotape } from "lucide-react";
 import { DeviceEditButton } from "./device-edit-button";
 
 export const DeviceCard = (props: DeviceShemaType) => {
@@ -19,6 +20,10 @@ export const DeviceCard = (props: DeviceShemaType) => {
         <CardHeader className="flex flex-row items-start bg-muted/50">
           <div className="grid gap-0.5">
             <CardTitle className="group flex items-center gap-2 text-lg">
+              {props.deviceType == DeviceType.Controller && <TowerControl />}
+              {props.deviceType == DeviceType.CCTV_Camera && <Cctv />}
+              {props.deviceType == DeviceType.Detector && <Radar />}
+              {props.deviceType == DeviceType.NVR && <Videotape />}
               {props.deviceType}
               <Button
                 size="icon"
@@ -61,6 +66,12 @@ export const DeviceCard = (props: DeviceShemaType) => {
                 <span className="text-muted-foreground">Serial Number</span>
                 <span>{props.serialNumber}</span>
               </li>
+              {props.deviceType != DeviceType.Controller && (
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Pin</span>
+                  <span>{props.pin > 0 ? props.pin : "Virtual"}</span>
+                </li>
+              )}
             </ul>
             <Separator className="my-2" />
             <ul className="grid gap-3">
@@ -79,22 +90,6 @@ export const DeviceCard = (props: DeviceShemaType) => {
           <div className="text-xs text-muted-foreground">
             Last updated <time dateTime="2023-11-23">November 23, 2023</time>
           </div>
-          {/* <Pagination className="ml-auto mr-0 w-auto">
-          <PaginationContent>
-          <PaginationItem>
-          <Button size="icon" variant="outline" className="h-6 w-6">
-          <ChevronLeftIcon className="h-3.5 w-3.5" />
-          <span className="sr-only">Previous Device</span>
-          </Button>
-          </PaginationItem>
-          <PaginationItem>
-          <Button size="icon" variant="outline" className="h-6 w-6">
-          <ChevronRightIcon className="h-3.5 w-3.5" />
-          <span className="sr-only">Next Device</span>
-          </Button>
-          </PaginationItem>
-          </PaginationContent>
-          </Pagination> */}
         </CardFooter>
       </Card>
     </>
