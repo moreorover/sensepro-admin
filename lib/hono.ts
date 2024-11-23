70a92ef279c5bfb34888576c5f7696d27977c493
moreorover <moreorover@null.computer>
2024-11-23 20:57:11 +0200

    fix: fixing hono client

--- lib/hono.ts
+++ lib/hono.ts
@@ -1,6 +1,6 @@
 import {AppType} from "@/app/api/[[...route]]/route";
 import {hc} from "hono/client";
 
-console.log(`Initialising Hono client with NEXT_PUBLIC_APP_URL: ${process.env.NEXT_PUBLIC_APP_URL}`)
+console.log(`Initialising Hono client with NEXT_PUBLIC_APP_URL: ${process.env.APP_URL}`)
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
+export const client = hc<AppType>(process.env.APP_URL!);
