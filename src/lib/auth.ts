a15bc3fa24672247b5c2d2a73bf43b37e6fe4bb1
Martin <mselvenis@gmail.com>
2025-01-09 13:35:21 +0000

    fix: next cookies

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,5 +11,6 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: false,
   },
+  plugins: [nextCookies()],
   trustedOrigins: [process.env.NEXT_PUBLIC_APP_URL as string],
 });
